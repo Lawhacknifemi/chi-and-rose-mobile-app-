b21217a244266cbd73600534bddc406171c9f068
Sherif Lawal <lawalsheriffnifemi@gmail.com>
2026-02-12 07:19:46 +0100

    fix: Add missing imports for ImageFilter and GoogleFonts in WellnessHeroCard.

--- apps/app/lib/presentation/ui/home/widgets/wellness_hero_card.dart
+++ apps/app/lib/presentation/ui/home/widgets/wellness_hero_card.dart
@@ -1,4 +1,6 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WellnessHeroCard extends StatelessWidget {
   final String? title;
